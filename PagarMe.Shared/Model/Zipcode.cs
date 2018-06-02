b7073b748c326568389f7eee4b6e6102b3786228
Jonatas Maximiano <maximianojonatas@gmail.com>
2018-06-02 16:06:07 -0300

    Add zipcode check method

---
+++ PagarMe.Shared/Model/Zipcode.cs
@@ -0,0 +1,50 @@
+﻿using System;
+namespace PagarMe
+{
+    public class Zipcode : Base.Model
+    {
+        protected override string Endpoint { get { return "/zipcodes"; } }
+       
+        public Zipcode()
+            : this(null)
+        {
+        }
+
+        public Zipcode(PagarMeService service)
+            : base(service)
+        {
+        }
+
+        public string Street
+        {
+            get { return GetAttribute<string>("street"); }
+        }
+
+        public string Neighborhood
+        {
+            get { return GetAttribute<string>("neighborhood"); }
+        }
+
+        public string City
+        {
+            get { return GetAttribute<string>("city"); }
+        }
+
+        public string State
+        {
+            get { return GetAttribute<string>("state"); }
+        }
+
+        public string zipcode
+        {
+            get { return GetAttribute<string>("zipcode"); }
+            set { SetAttribute("zipcode", value); }
+        }
+
+        public void CheckZipcode(String zipcode){
+            var request = CreateRequest("GET", zipcode);
+            ExecuteSelfRequest(request);
+        }
+
+    }
+}
---
+++ PagarMe.Tests/ZipcodeTest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using NUnit.Framework;
+namespace PagarMe.Tests
+{
+    [TestFixture]
+    public class ZipcodeTests : PagarMeTestFixture{
+        [Test]
+        public void ZipcodeTest()
+        {
+            Zipcode zipcode = new Zipcode();
+            zipcode.CheckZipcode("04250000");
+            Assert.AreEqual("São Paulo",zipcode.City);
+            Assert.AreEqual("SP",zipcode.State);
+            Assert.AreEqual("Rua Budapeste",zipcode.City);
+            Assert.AreEqual("Vila Marte", zipcode.Neighborhood);
+        } 
+    }
+      
+
+}
