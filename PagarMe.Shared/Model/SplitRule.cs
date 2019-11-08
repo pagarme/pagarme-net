b6de89013c72ea101d25803d4107485802eb3a4e
Jonatas Maximiano <maximianojonatas@gmail.com>
2019-11-08 12:01:29 -0300

    Fix some parameters from Split Rule and Add Analyzing status

--- PagarMe.Shared/Model/SplitRule.cs
+++ PagarMe.Shared/Model/SplitRule.cs
@@ -61,13 +61,25 @@ namespace PagarMe
 			set { SetAttribute ("liable", value); }
 		}
 
-		public int Percentage {
-			get { return GetAttribute<int> ("percentage"); }
+		public int? Percentage {
+			get { 
+					try{
+						return GetAttribute<int> ("percentage"); 
+					} catch(Exception){
+						return null;
+					}
+				}
 			set { SetAttribute ("percentage", value); }
 		}
 
 		public int Amount {
-			get { return GetAttribute<int> ("amount"); }
+			get { 
+					try{
+						return GetAttribute<int> ("amount"); 
+					} catch (Exeception){
+						return null;
+					}
+				}
 			set { SetAttribute ("amount", value); }
 		}
 
