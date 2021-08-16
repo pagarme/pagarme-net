8dbcf4636c58849cc3316658d5ba25beb10f64fa
Giovanni Rodrigues Pereira Scaduto <giovanni.scaduto@outlook.com>
2021-08-16 17:30:01 -0300

    payable status

--- PagarMe.Shared/Enumeration/PayableStatus.cs
+++ PagarMe.Shared/Enumeration/PayableStatus.cs
@@ -1,14 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace PagarMe
+﻿namespace PagarMe
 {
     public enum PayableStatus
     {
         [Base.EnumValue("waiting_funds")]
         WaitingFunds,
-        [Base.EnumValue("paid")] Paid,
-        [Base.EnumValue("suspended")] Suspended
+        [Base.EnumValue("paid")]
+        Paid,        
+        [Base.EnumValue("prepaid")]
+        Prepaid
     }
 }
